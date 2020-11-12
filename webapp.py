9cdebf1515a25b2f7f44d3af9193b015376f4258
george <g_ovesby@hotmail.co.uk>
2020-11-12 23:30:04 +0000

    updated notebooks + minor webapp improvements

--- webapp.py
+++ webapp.py
@@ -2,6 +2,7 @@ import streamlit as st
 from src.ingredient_tokenizer import load_tokenizer, get_tokenizer_vocab
 import tfidf_matcher as tm
 from src.ingredient_tokenizer import load_tokenizer
+from src.non_ingredients import get_non_ingredients
 from transformers import pipeline
 import tokenizers
 from pathlib import Path
@@ -23,6 +24,8 @@ def load_model():
 
 def normalise_recipe(recipe):
     ingredients = get_tokenizer_vocab(load_tokenizer("artifacts"))
+    non_ingredients = get_non_ingredients(ingredients)
+    ingredients = [i for i in ingredients if i not in non_ingredients]
     matched_recipe = tm.matcher(recipe, ingredients, k_matches=1)["Lookup 1"].to_list()
     return matched_recipe
 
@@ -31,8 +34,17 @@ CUMULATIVE_CUTOFF = st.sidebar.slider('cumulative  probability cut-off',0.0,1.0,
 fill_mask = load_model()
 recipe_messy = st.text_area('Recipe:')
 if recipe_messy:
-    recipe_normalised = normalise_recipe(recipe_messy.split(','))
-    'Normalised recipe: ', ', '.join(recipe_normalised)
+    if recipe_messy.count(',') > recipe_messy.count('\n'):
+        messy_ingredients = recipe_messy.split(',')
+    else:
+        messy_ingredients = recipe_messy.split('\n')
+    messy_ingredients = [m.replace(' ', '_').replace('finely_chopped', '')
+    for m in messy_ingredients if m]
+
+    recipe_normalised = normalise_recipe(messy_ingredients)
+
+    'Normalised recipe: '
+    st.write(', '.join(recipe_normalised))
     st.write('\n')
     result = fill_mask(' '.join(recipe_normalised) + ' <mask>')
     cumulative_probability = 0
